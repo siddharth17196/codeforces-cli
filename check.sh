bd52e753bd42b78796747ffdb0d2e07480370c48
siddharth17196 <siddharth17196@iiitd.ac.in>
2020-04-25 18:48:54 +0530

    updates

--- check.sh
+++ check.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-echo "filename : $1"
+# echo "filename : $1"
 file="$1"
-if [[ file == *.py ]]
+if [[ $file == *.py ]]
 then
-    echo "python file"
-    python "$file" <inp1.txt >o.txt
-    nvim o.txt out1.txt -O
+    echo "Lang : Python"
+    python "$file" < inp1.txt > o.txt
 elif [[ $file == *.cpp ]]
 then
-    echo "C++ file"
+    echo "Lang : C++"
     g++ "$file" 
-    ./a.out <i.txt >o.txt
-    nvim ot.txt o.txt -O
+    ./a.out < i.txt > o.txt
+fi
+# nvim o.txt out1.txt -O
+diff o.txt out1.txt >sam.txt  2>/dev/null
+if [[ "$?" -ne 0 ]]
+then
+    echo -e "Verdict : \e[0;31mWrong Answer\e[0m"
+    cat sam.txt
+else
+    echo -e "Verdict : \e[0;32mAC\e[0m"
+    # echo "AC"
 fi
