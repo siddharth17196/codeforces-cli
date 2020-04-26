8fe867f11a327bf577cf017505d5f835220eba63
siddharth17196 <siddharth17196@iiitd.ac.in>
2020-04-26 23:40:02 +0530

    fixes

--- scripts/scrapq.py
+++ scripts/scrapq.py
@@ -15,38 +15,37 @@ if page.status_code == 200:
     io = ['output', 'input']
     problems = [k.get_text() for k in text_data]
     test_cases = [k.get_text() for k in test_cases]
-    intervals = []
-    count_tests = 0
-    dirname = []
+    test_case_intervals = 0
+    intervals = []  # has indices to separate test cases for each problem
+    dirname = []    # name of the problem subdirectories
     '''
         Create problem sub directories
     '''
+    test_case_intervals = 0 # count number of test cases for each problem
     for k in problems:
-        count_tests += 1
-        if k.lower() not in io:
-            count_tests -= 1
-            intervals.append(count_tests)
-            count_tests = 0
+        test_case_intervals += 1
+        if k.lower() not in io: # not selecting the input/output
+            test_case_intervals -= 1
+            intervals.append(test_case_intervals)
             if os.path.isdir(contest+"/"+k[:k.index('.')].lower())==False:
                 os.mkdir(contest+"/"+k[:k.index('.')].lower())
             dirname.append(k[:k.index('.')].lower())
-    intervals.append(count_tests)
-    print(dirname)
+    intervals.append(test_case_intervals)
     '''
         Add inputs and outputs in the corresponding problem directories
     '''
+    test_case_selecter = 0  # iterate over test_cases 
     for i in range(1,len(intervals)):
-        j = 0
-        test_num = 0
-        while(j<intervals[i]):
-            test_num += 1
+        test_case_num = 0   # test case number for each problem
+        while(testcase_num<intervals[i]):
+            test_case_num += 1
             problem_dir = os.path.join(contest, dirname[i-1])
-            test_input = problem_dir + "/inp" + str(test_num) + ".txt"
-            test_output = problem_dir + "/out" + str(test_num) + ".txt"
+            test_input = problem_dir + "/inp" + str(test_case_num) + ".txt"
+            test_output = problem_dir + "/out" + str(test_case_num) + ".txt"
             with open(test_input, "w") as f:
-                f.write(test_cases[j][1:])
+                f.write(test_cases[test_case_selecter][1:])
             with open(test_output, "w") as f:
-                f.write(test_cases[j+1][1:])
-            j += 2
+                f.write(test_cases[test_case_selecter+1][1:])
+            test_case_selecter += 2   # as input output are in pairs of 2
 else:
     print('Contest Not Available')
